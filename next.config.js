e863597ec8c64ec7bfa3f8ba8f3840b7e7cdc254
jhm69 <jahangirhmarup@gmail.com>
2023-07-14 09:16:52 +0600

    ignored eslint

--- next.config.js
+++ next.config.js
@@ -25,6 +25,11 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
